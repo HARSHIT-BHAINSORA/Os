a3e26b4362f6cffb098ad9887c687a319bf02169
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-10-03 05:30:30 -0400

    fcfs implemnetation

---
+++ LAB2/FCFS.c
@@ -0,0 +1,89 @@
+// fcfs
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Process{
+    int pid; // process id
+    int at; // arrival id 
+    int bt; // burst time
+    int ct; // compiletion time 
+    int tat; // trun round time 
+    int wt ; // waiting time 
+    int rt; // response time
+    int st; // strat time 
+};
+
+
+int compare(const void *p1 , const void *p2){
+    int a = ((struct Process *) p1)->at;
+    int b = ((struct Process *) p2)->at;
+    
+    if(a < b)
+        return -1;
+    else
+        return 1;
+}
+
+
+int main(){
+    int n;
+    int sumWT = 0 , sumTat = 0 , sumrt = 0;
+    float avgrt = 0, avgWT = 0 , avgTat = 0;
+    
+    printf("Enter the number of processes : ");
+    scanf("%d" , &n);
+    
+    struct Process p[n];
+    
+    for(int i = 0 ; i < n ; i++){
+        
+        p[i].pid = i;
+        printf("Enter the Arrival Time and Burst time for process = %d : ",i);
+        scanf("%d%d", &p[i].at , &p[i].bt);
+    }
+    
+    qsort((void*)p , n , sizeof(struct Process) , compare);
+    
+    for(int i = 0 ; i < n ; i++){
+        if(i == 0){
+            p[i].st = p[i].at;
+            p[i].ct = p[i].st + p[i].bt;
+            p[i].rt = 0;
+        }
+        
+        if(p[i-1].ct < p[i].at){
+            p[i].st = p[i].at;
+            p[i].ct = p[i].st + p[i].bt;
+            p[i].rt = 0;
+        }
+        else
+        {
+            p[i].st = p[i-1].ct;
+            p[i].ct = p[i].st + p[i].bt;
+            p[i].rt = p[i].st-p[i].at;
+        }
+        p[i].tat = p[i].ct-p[i].at;
+        p[i].wt = p[i].tat-p[i].bt;
+        
+        sumWT += p[i].wt;
+        sumTat +=p[i].tat;
+        sumrt += p[i].rt;
+        
+    }
+    
+    avgWT = sumWT/n;
+    avgTat = sumTat/n;
+    avgrt = sumrt/n;
+    
+    
+    printf("process\tat\tbt\tst\tct\trt\ttat\twt\n");
+    
+    for(int i = 0 ; i < n ; i++){
+        printf("%d\t\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",p[i].pid , p[i].at , p[i].bt , p[i].st , p[i].ct , p[i].rt , p[i].tat , p[i].wt);
+    }
+    
+    printf("Avg wt , tat , rt %d %d %d" , avgWT , avgTat , avgrt);
+    
+    
+}

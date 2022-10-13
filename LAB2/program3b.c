cab9d168348376318991b902c81dddf697bb473b
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-10-13 08:39:46 -0400

    Orphan process

---
+++ LAB2/program3b.c
@@ -0,0 +1,19 @@
+// Orphan process
+
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    
+    int p = fork();
+    
+    if(p == 0){
+        printf("The child pid is %d\n" , getpid());
+        printf("The child parent pid is %d\n" , getppid());
+    }
+    else{
+        sleep(5);
+        printf("The parent process pid is %d\n" , getpid());
+    }
+    
+}

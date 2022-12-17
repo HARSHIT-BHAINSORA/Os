e68eb39044f3dd5db2e9373b528c78e2ad1423e8
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-12-17 08:37:29 +0530

    Pattern 2

---
+++ ShellPrograming/P2.sh
@@ -0,0 +1,14 @@
+echo "Enter the number of Rows = "
+read rows
+for(( i=1; i<=rows; i++ ));
+do
+	for(( j=1; j<=rows - i; j++ ))
+	do
+		echo -n "  ";
+	done
+	for(( j=1; j<=2*i -1; j++ ))
+	do
+		echo -n "* "
+	done
+	echo
+done	

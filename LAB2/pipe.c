9fddecdd0721bd1d89defa62142539c1e42fd866
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-12-18 16:42:02 +0530

    Pipe System call implementation

---
+++ LAB2/pipe.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    int p[2];
+    int returnstatus;
+    char writemsg[2][25] = {"Operating" , "System"};
+    char readmsg[25];
+    
+    returnstatus = pipe(p);
+    
+    if(returnstatus == 1)
+    {
+        printf("Pipe is not Created...");
+    }
+    else
+    {
+        printf("Writing to pipe msg = %s\n" , writemsg[1]);
+        write(p[1], writemsg[1] , sizeof(writemsg[1]));
+        read(p[0] , readmsg , sizeof(readmsg));
+        
+        printf("Reading from pipe - msg = %s", readmsg);
+        return 0;
+    }
+    
+}
